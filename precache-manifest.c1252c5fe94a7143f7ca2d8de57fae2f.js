c4bba52e55dbcf89037a0e19ab6cf1900ca9ede7
saket2508 <saketns@gmail.com>
2020-12-15 11:02:42 +0530

    Updates

---
+++ precache-manifest.c1252c5fe94a7143f7ca2d8de57fae2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00447773e6e7d7bca56f1b41618ba0a",
+    "url": "/React-Unsplash-Gallery-App/index.html"
+  },
+  {
+    "revision": "03c1635edc77d9e91ee1",
+    "url": "/React-Unsplash-Gallery-App/static/css/main.d6482b25.chunk.css"
+  },
+  {
+    "revision": "90027a568d0cf3405384",
+    "url": "/React-Unsplash-Gallery-App/static/js/2.e1f996b7.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/React-Unsplash-Gallery-App/static/js/2.e1f996b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c1635edc77d9e91ee1",
+    "url": "/React-Unsplash-Gallery-App/static/js/main.953b3af3.chunk.js"
+  },
+  {
+    "revision": "4c7f6d7ce69c90b86d56",
+    "url": "/React-Unsplash-Gallery-App/static/js/runtime-main.ca0e6272.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Unsplash-Gallery-App/precache-manifest.c1252c5fe94a7143f7ca2d8de57fae2f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Unsplash-Gallery-App/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
